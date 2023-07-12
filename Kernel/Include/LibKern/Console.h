176fcb701918991ca5534310e3c22a8ea03a804f
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-07-12 18:12:36 +0300

    Headers now have their own directories.

---
+++ Kernel/Include/LibKern/Console.h
@@ -0,0 +1,21 @@
+/*
+ * Console printing/logging functionalities for the Kernel
+ *
+ * Original src: mit-pdos/xv6-public
+ * Modified by: Tuna CICI
+ */
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include <stdint.h>
+#include <stdarg.h>
+
+void kprint_uint(uint64_t uval, uint8_t base);
+void kprint_int(int64_t val, uint8_t base);
+void kprint_str(const char *str);
+
+void kprintf(const char *fmt, ...);
+void klog(const char *fmt, ...);
+
+#endif /* CONSOLE_H */
---
+++ Kernel/Include/LibKern/Time.h
@@ -0,0 +1,20 @@
+/*
+ * Time & timing related functionalities for the Kernel (mostly arch dependent)
+ *
+ * Author: Tuna CICI
+ */
+
+#ifndef TIME_H
+#define TIME_H
+
+#define NANO_PER_SEC  1000000000 /* 10^9 seconds */
+#define MICRO_PER_SEC 1000000 /* 10^6 seconds */
+#define MILLI_PER_SEC 1000 /* 10^3 seconds */
+
+#define NANO_PER_MICRO 1000 /* 10^3 microseconds */
+
+#include <stdint.h>
+
+uint64_t arm64_uptime(void);
+
+#endif /* TIME_H */
