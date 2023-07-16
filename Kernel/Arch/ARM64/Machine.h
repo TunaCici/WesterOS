26f83ae31cfd5f9564546562d6537cbb6cd358b6
TunaCici <36923239+TunaCici@users.noreply.github.com>
2023-07-16 14:45:04 +0300

    Wrap machine-code around C functions.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

---
+++ Kernel/Arch/ARM64/Machine.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * Original Source: seL4/include/arch/arm/armv/armv8-a/64/armv/machine.h
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#pragma once
+
+static inline void wfi(void)
+{
+    asm volatile("wfi" ::: "memory");
+}
+
+static inline void dsb(void)
+{
+    asm volatile("dsb sy" ::: "memory");
+}
+
+static inline void dsb_ishst(void)
+{
+    asm volatile("dsb ishst" ::: "memory");
+}
+
+static inline void dmb(void)
+{
+    asm volatile("dmb sy" ::: "memory");
+}
+
+static inline void isb(void)
+{
+    asm volatile("isb sy" ::: "memory");
+}
+
+#define MRS(reg, v)  asm volatile("mrs %x0," reg : "=r"(v))
+#define MSR(reg, v)                                \
+    do {                                           \
+        word_t _v = v;                             \
+        asm volatile("msr " reg ",%x0" :: "r" (_v));\
+    }while(0)
+
+#define SYSTEM_WRITE_WORD(reg, v) MSR(reg, v)
+#define SYSTEM_READ_WORD(reg, v)  MRS(reg, v)
+#define SYSTEM_WRITE_64(reg, v)   MSR(reg, v)
+#define SYSTEM_READ_64(reg, v)    MRS(reg, v)
