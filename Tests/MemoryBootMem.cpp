e04094113c9efadcf6f5f7853a0ee16dd811d82f
TunaCici <36923239+TunaCici@users.noreply.github.com>
2024-01-23 21:19:57 +0300

    test: Housekeeping for BootMem.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- Tests/MemoryBootMem.cpp
+++ Tests/MemoryBootMem.cpp
@@ -32,12 +32,12 @@ TEST(MemoryBootMem, alloc)
 
         /* stupid request - 0 */
         void *tmp = bootmem_alloc(0);
-        EXPECT_TRUE(tmp == 0);
+        EXPECT_TRUE(tmp == nullptr);
 
 
         /* more than what's allowed */
         tmp = bootmem_alloc(BM_ARENA_SIZE + 1);
-        EXPECT_TRUE(tmp == 0);
+        EXPECT_TRUE(tmp == nullptr);
 
         /* simple */
         tmp = bootmem_alloc(1);
@@ -55,7 +55,7 @@ TEST(MemoryBootMem, alloc)
 
         /* no more space;( */
         tmp = bootmem_alloc(1);
-        EXPECT_TRUE(tmp == 0);
+        EXPECT_TRUE(tmp == nullptr);
 
         /* addresses are valid? */
         for (auto i = 0; i < BM_ARENA_SIZE * PAGE_SIZE; i++) {
