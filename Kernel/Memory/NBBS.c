c800d22b53694ffe22f2a05a8daf34ee00b969a3
TunaCici <36923239+TunaCici@users.noreply.github.com>
2024-05-10 18:33:33 +0300

    pmm: Reorganized NBBS metadata.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- Kernel/Memory/NBBS.c
+++ Kernel/Memory/NBBS.c
@@ -16,28 +16,36 @@
 static volatile uint8_t *tree = 0;
 static volatile uint32_t *index = 0;
 
+static volatile uint32_t depth = 0;
 static volatile uint64_t tree_size = 0; /* bytes */
 static volatile uint64_t index_size = 0; /* bytes */
 
-/*
- * Maximum allowed alloc size is: 2^(max_level - 1) * min_size
- * Naturally max_allowed <= total_memory
-*/
-static volatile uint32_t max_level = 10;
-
-static volatile uint64_t min_size = PAGE_SIZE;
+static volatile uint64_t total_memory = 0;
+static volatile uint64_t max_level = 0; /* [0, depth] */
+static volatile uint64_t min_size = 0;
 static volatile uint64_t max_size = 0;
 
 int nb_init(uint64_t base_addr, uint64_t size)
 {
+        if (size < min_size) {
+                return 1;
+        }
+
         if (base_addr == 0 | size == 0) {
                 return 1;
         }
 
+        /* Setup */
+        total_memory = size;
+        min_size = PAGE_SIZE;
+        depth = LOG2(total_memory / min_size) - 1;
+        max_level = 0;
+        max_size = (EXP2(depth - max_level) * min_size);
+
         /* Calculate required tree size */
         uint32_t total_nodes = 1; // we start at idx 1
-        for (uint32_t i = 0; i < max_level; i++) {
-                total_nodes += (size / ((0x1 << i) * min_size));
+        for (uint32_t i = 0; i < depth; i++) {
+                total_nodes += (size / (EXP2(i) * min_size));
         }
 
         /* Calculate required index size */
