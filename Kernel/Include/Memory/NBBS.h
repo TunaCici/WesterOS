c621a7c321834169f6cfbe02a594923d9b553d34
TunaCici <36923239+TunaCici@users.noreply.github.com>
2024-06-01 23:52:24 +0300

    nbbs: Resync with the main repository.
    Signed-off-by: TunaCici <36923239+TunaCici@users.noreply.github.com>

--- Kernel/Include/Memory/NBBS.h
+++ Kernel/Include/Memory/NBBS.h
@@ -1,15 +1,23 @@
 /*
- * Non-Blocking Buddy System - To be used to the new physical memory mngr.
+ * Non-Blocking Buddy System definitions
  *
  * As described in:
  * "A Non-blocking Buddy System for Scalable Memory Allocation on Multi-core 
  * Machines by" R. Marotta, M. Ianni, A. Scarselli, A. Pellegrini and F. Quaglia
  *
  * Reference:
+ * https://ieeexplore.ieee.org/document/9358002
  * https://github.com/HPDCS/NBBS
  *
  * Author: Tuna CICI
  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef NBBS_H
+#define NBBS_H
 
 #include <stdint.h>
 
@@ -30,19 +38,40 @@
 #define OCC             ((uint8_t) 0x10)
 #define BUSY            (OCC | OCC_LEFT | OCC_RIGHT)
 
+/*
+ * Configuration
+ */
+
+#define NB_MIN_SIZE 4096ULL /* bytes */
+#define NB_MAX_ORDER 9U
+#define NB_MALLOC(size) bootmem_alloc(size)
+
+/*
+ * Math functions
+ */
+
 #define EXP2(n) (0x1ULL << (n))
 #define LOG2_LOWER(n) (64ULL - __builtin_clzll(n) - 1ULL) // 64 bit
 
-#define FAD(ptr, val) __atomic_add_fetch(ptr, val, __ATOMIC_SEQ_CST)
-#define BCAS(ptr, expected, desired) __atomic_compare_exchange_n(ptr, expected, desired, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
-#define VCAS(ptr, expected, desired) __atomic_compare_exchange_n(ptr, expected, desired, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST) ? (*expected) : 0
+/*
+ * Atomic operations:
+ *
+ * FAD: Fetch-And-Decrement
+ * BCAS: Binary-Compare-And-Swap
+ * VCAS: Value-Compare-And-Swap
+ */
 
-#define LEVEL(n) LOG2_LOWER(n)
+#define FAD(ptr, val) \
+        __atomic_add_fetch(ptr, val, __ATOMIC_SEQ_CST)
+#define BCAS(ptr, expected, desired) \
+        __atomic_compare_exchange_n(ptr, expected, desired, 0, \
+                __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
+#define VCAS(ptr, expected, desired) \
+        __atomic_compare_exchange_n(ptr, expected, desired, 0, \
+                __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST) ? (*expected) : 0
 
 /*
  * Public APIs
- *
- * TODO: Explain.
  */
 
 int  nb_init(uint64_t base_addr, uint64_t size);
@@ -51,83 +80,88 @@ void nb_free(void *addr);
 
 /*
  * Private APIs
- *
- * TODO: Explain.
  */
 
-uint32_t __try_alloc(uint32_t node);
-void __freenode(uint32_t node, uint32_t upper_bound);
-void __unmark(uint32_t node, uint32_t upper_bound);
+uint32_t __nb_try_alloc(uint32_t node);
+void __nb_freenode(uint32_t node, uint32_t upper_bound);
+void __nb_unmark(uint32_t node, uint32_t upper_bound);
 
-uint64_t __block_size(uint32_t level);
-void __clean_block(void* addr, uint64_t size);
+uint32_t __nb_leftmost(uint32_t node, uint32_t depth);
+void __nb_clean_block(void* addr, uint64_t size);
 
 /*
- * Diagnostics
- *
- * TODO: Explain.
+ * Statistics
  */
 
+uint64_t nb_stat_min_size();
+uint32_t nb_stat_max_order();
+
+uint64_t nb_stat_tree_size();
+uint64_t nb_stat_index_size();
+uint32_t nb_stat_depth();
+uint32_t nb_stat_base_level();
+uint64_t nb_stat_max_size();
+uint32_t nb_stat_release_count();
+
+uint64_t nb_stat_total_memory();
+uint64_t nb_stat_used_memory();
+
+uint64_t nb_stat_block_size(uint32_t order);
+uint64_t nb_stat_total_blocks(uint32_t order);
+uint64_t nb_stat_used_blocks(uint32_t order);
+
+uint8_t nb_stat_occupancy_map(uint8_t *buff, uint32_t order);
 
 /*
  * Helpers
- *
- * TODO:? Explain.
  */
-static inline uint8_t mark(uint8_t val, uint32_t child)
+static inline uint8_t nb_mark(uint8_t val, uint32_t child)
 {
         return ((uint8_t) (val | (OCC_LEFT >> (child % 2))));
 }
 
-static inline uint8_t unmark(uint8_t val, uint32_t child)
+static inline uint8_t nb_unmark(uint8_t val, uint32_t child)
 {
-        return ((uint8_t) (val & !((OCC_LEFT | COAL_LEFT) >> (child % 2))));
+        return ((uint8_t) (val & ~((OCC_LEFT | COAL_LEFT) >> (child % 2))));
 }
 
-static inline uint8_t set_coal(uint8_t val, uint32_t child)
+static inline uint8_t nb_set_coal(uint8_t val, uint32_t child)
 {
         return ((uint8_t) (val | (COAL_LEFT >> (child % 2))));
 }
 
-static inline uint8_t clean_coal(uint8_t val, uint32_t child)
+static inline uint8_t nb_clean_coal(uint8_t val, uint32_t child)
 {
-        return ((uint8_t) (val & !(COAL_LEFT >> (child % 2))));
+        return ((uint8_t) (val & ~(COAL_LEFT >> (child % 2))));
 }
 
-static inline uint8_t is_coal(uint8_t val, uint32_t child)
+static inline uint8_t nb_is_coal(uint8_t val, uint32_t child)
 {
         return ((uint8_t) (val & (COAL_LEFT >> (child % 2))));
 }
 
-static inline uint8_t is_occ_buddy(uint8_t val, uint32_t child)
+static inline uint8_t nb_is_occ_buddy(uint8_t val, uint32_t child)
 {
         return ((uint8_t) (val & (OCC_RIGHT << (child % 2))));
 }
 
-static inline uint8_t is_coal_buddy(uint8_t val, uint32_t child)
+static inline uint8_t nb_is_coal_buddy(uint8_t val, uint32_t child)
 {
         return ((uint8_t) (val & (COAL_RIGHT << (child % 2))));
 }
 
-static inline uint8_t is_free(uint8_t val)
+static inline uint8_t nb_is_free(uint8_t val)
 {
         return !(val & BUSY);
 }
 
-/* TODO: Ugly code; refactor */
-static inline uint32_t leftmost(uint32_t node, uint32_t depth)
+static inline uint32_t nb_level(uint32_t node)
 {
-        /* Index to level */
-        uint32_t level = LOG2_LOWER(node);
-
-        /* Size (in terms of leaf size) */
-        uint64_t block_size = EXP2(depth - level);
-
-        /* Offset within level */
-        uint64_t offset = node % EXP2(level);
-
-        /* Leftmost leaf */
-        uint32_t leftmost_leaf = EXP2(depth) + offset * block_size;
-
-        return leftmost_leaf;
+        return LOG2_LOWER(node);
 }
+
+#endif /* NBBS_H */
+
+#ifdef __cplusplus
+}
+#endif
--- Kernel/Memory/NBBS.c
+++ Kernel/Memory/NBBS.c
@@ -2,9 +2,6 @@
  * This file (NBSS.c) implements the Non-Blocking Buddy System
  */
 
-#ifndef NBBS_H
-#define NBBS_H
-
 #include <stdint.h>
 
 #include "LibKern/String.h"
@@ -13,76 +10,77 @@
 #include "Memory/BootMem.h"
 #include "Memory/NBBS.h"
 
-static volatile uint8_t *tree = 0;
-static volatile uint32_t *index = 0;
+/* Meta-data */
+static uint8_t *nb_tree = 0;
+static uint32_t *nb_index = 0;
 
-static volatile uint64_t tree_size = 0; /* bytes */
-static volatile uint64_t index_size = 0; /* bytes */
+static uint64_t nb_tree_size = 0; /* bytes */
+static uint64_t nb_index_size = 0; /* bytes */
 
-static volatile uint64_t base_address = 0;
-static volatile uint64_t total_memory = 0;
-static volatile uint32_t depth = 0;
-static volatile uint64_t base_level = 0;
-static volatile uint64_t min_size = 0;
-static volatile uint64_t max_size = 0;
-static volatile uint64_t max_order = 0;
-static volatile uint32_t release_count = 0;
+static uint64_t nb_base_address = 0;
+static uint64_t nb_total_memory = 0;
+static uint32_t nb_depth = 0;
+static uint64_t nb_base_level = 0;
+static uint64_t nb_max_size = 0;
+static uint32_t nb_release_count = 0;
+
+/* Statistics */
+static uint64_t nb_stat_alloc_blocks[NB_MAX_ORDER + 1] = {0};
 
 int nb_init(uint64_t base, uint64_t size)
 {
-        if (base == 0 | size == 0) {
+        if (base == 0 || size == 0) {
                 return 1;
         }
 
-        if (size < min_size) {
+        if (size < NB_MIN_SIZE) {
                 return 1;
         }
 
         /* Setup */
-        base_address = base;       
-        total_memory = size;
-        min_size = PAGE_SIZE;
-        depth = LOG2_LOWER(total_memory / min_size);
-        max_order = 9;
-        max_size = EXP2(max_order) * min_size;
-        base_level = depth - max_order;
+        nb_base_address = base;       
+        nb_total_memory = size;
+        
+        nb_depth = LOG2_LOWER(nb_total_memory / NB_MIN_SIZE);
+        nb_base_level = nb_depth - NB_MAX_ORDER;
+        nb_max_size = EXP2(NB_MAX_ORDER) * NB_MIN_SIZE;
 
         /* Calculate required tree size - root node is at index 1  */
-        uint32_t total_nodes = EXP2(depth + 1);
+        uint32_t total_nodes = EXP2(nb_depth + 1);
 
         /* Calculate required index size */
-        uint32_t total_pages = (total_memory / min_size);
+        uint32_t total_pages = (nb_total_memory / NB_MIN_SIZE);
 
-        tree_size = total_nodes * 1;  // each node is 1 byte
-        index_size = total_pages * 4; // each page id is 4 byte
+        nb_tree_size = total_nodes * 1;  // each node is 1 byte
+        nb_index_size = total_pages * 4; // each leaf index is 4 byte
 
         /* Allocate */
-        uint32_t req_pages = (tree_size + PAGE_SIZE - 1) / PAGE_SIZE;
-        tree = (uint8_t*) bootmem_alloc(req_pages);
-
-        if (!tree) {
+        nb_tree = (uint8_t*) NB_MALLOC(nb_tree_size);
+        if (!nb_tree) {
                 return 1;
-        } 
+        }
 
-        req_pages = (index_size + PAGE_SIZE - 1) / PAGE_SIZE;
-        index = (uint32_t*) bootmem_alloc(req_pages);
+        nb_index = (uint32_t*) NB_MALLOC(nb_index_size);
 
-        if (!index) {
+        if (!nb_index) {
                 return 1;
         }
 
         /* Initialize */
-        memset((void*) tree, 0x0, tree_size);
-        memset((void*) index, 0x0, index_size);
+        memset((void*) nb_tree, 0x0, nb_tree_size);
+        memset((void*) nb_index, 0x0, nb_index_size);
+        memset((void*) nb_stat_alloc_blocks, 0x0, ((NB_MAX_ORDER + 1) * 8));
+        // ------------------------------------------- sizeof(uint64_t) ^
+        nb_release_count = 0;
 
         return 0;
 }
 
-uint32_t __try_alloc(uint32_t node)
+uint32_t __nb_try_alloc(uint32_t node)
 {
         /* Occupy the node */
         uint8_t free = 0;
-        if (!BCAS(&tree[node], &free, BUSY)) {
+        if (!BCAS(&nb_tree[node], &free, BUSY)) {
                 return node;
         }
 
@@ -90,7 +88,7 @@ uint32_t __try_alloc(uint32_t node)
         uint32_t child = 0;
 
         /* Propagate the info about the occupancy up to the ancestor node(s) */
-        while (base_level < LEVEL(current)) {
+        while (nb_base_level < nb_level(current)) {
                 child = current;
                 current = current >> 1;
 
@@ -98,37 +96,64 @@ uint32_t __try_alloc(uint32_t node)
                 uint8_t new_val = 0;
 
                 do {
-                        curr_val = tree[current];
+                        curr_val = nb_tree[current];
 
                         if (curr_val & OCC) {
-                                __freenode(node, LEVEL(child));
+                                __nb_freenode(node, nb_level(child));
                                 return current;
                         }
 
-                        new_val = clean_coal(curr_val, child);
-                        new_val = mark(new_val, child);
-                } while (!BCAS(&tree[current], &curr_val, new_val));
+                        new_val = nb_clean_coal(curr_val, child);
+                        new_val = nb_mark(new_val, child);
+                } while (!BCAS(&nb_tree[current], &curr_val, new_val));
         }
 
         return 0;
 }
 
+/* TODO: Ugly code; refactor */
+uint32_t __nb_leftmost(uint32_t node, uint32_t depth)
+{
+        /* Index to level */
+        uint32_t level = nb_level(node);
+
+        /* Size (in terms of leaf size) */
+        uint64_t block_size = EXP2(depth - level);
+
+        /* Offset within level */
+        uint64_t offset = node % EXP2(level);
+
+        /* Leftmost leaf */
+        uint32_t leftmost_leaf = EXP2(depth) + offset * block_size;
+
+        return leftmost_leaf;
+}
+
+void __nb_clean_block(void* addr, uint64_t size)
+{
+        if (!addr || !size) {
+                return;
+        }
+
+        memset(addr, 0x0, size);
+}
+
 void* nb_alloc(uint64_t size)
 {
-        if (max_size < size) {
+        if (nb_max_size < size) {
                 return 0;
         }
 
-        if (size < min_size) {
-                size = min_size;
+        if (size < NB_MIN_SIZE) {
+                size = NB_MIN_SIZE;
         }
 
         nb_alloc_again:;
-        uint32_t ts = release_count;
-        uint32_t level = LOG2_LOWER(total_memory / size);
+        uint32_t ts = nb_release_count;
+        uint32_t level = LOG2_LOWER(nb_total_memory / size);
 
-        if (depth < level) {
-                level = depth;
+        if (nb_depth < level) {
+                level = nb_depth;
         }
 
         /* Range of nodes at target level */
@@ -136,35 +161,39 @@ void* nb_alloc(uint64_t size)
         uint32_t end_node = EXP2(level + 1);
 
         for (uint32_t i = start_node; i < end_node; i++) {
-                if (is_free(tree[i])) {
-                        uint32_t failed_at = __try_alloc(i);
+                if (nb_is_free(nb_tree[i])) {
+                        uint32_t failed_at = __nb_try_alloc(i);
 
                         if (!failed_at) {
                                 /* TODO: Explain what's going on here */
-                                uint32_t leaf = leftmost(i, depth) - EXP2(depth); 
-                                index[leaf] = i;
+                                uint32_t leaf = __nb_leftmost(
+                                        i, nb_depth) - EXP2(nb_depth);
+                                nb_index[leaf] = i;
+
+                                FAD(&nb_stat_alloc_blocks[nb_depth - level], 1);
                                 
-                                return (void*) (base_address + leaf * min_size);
+                                return (void*)
+                                        (nb_base_address + leaf * NB_MIN_SIZE);
                         } else {
                                 /* Skip the entire subtree [of failed] */
-                                uint32_t curr_level = LEVEL(i);
-                                uint32_t fail_level = LEVEL(failed_at);
+                                uint32_t curr_level = nb_level(i);
+                                uint32_t fail_level = nb_level(failed_at);
 
                                 uint32_t d = EXP2(curr_level - fail_level);
-                                i = (failed_at + 1) * d;
+                                i = ((failed_at + 1) * d) - 1;
                         }
                 }
         }
 
         /* A release occured, try again */
-        if (ts != release_count) {
+        if (ts != nb_release_count) {
                 goto nb_alloc_again;
         }
 
         return (void*) 0;
 }
 
-void __unmark(uint32_t node, uint32_t upper_bound)
+void __nb_unmark(uint32_t node, uint32_t upper_bound)
 {
         uint32_t current = node;
         uint32_t child = 0;
@@ -177,21 +206,22 @@ void __unmark(uint32_t node, uint32_t upper_bound)
                 current = current >> 1;
 
                 do {
-                        curr_val = tree[current];
+                        curr_val = nb_tree[current];
 
-                        if (!is_coal(curr_val, child)) {
+                        if (!nb_is_coal(curr_val, child)) {
                                 return;
                         }
                         
-                        new_val = unmark(curr_val, child);
-                } while (!BCAS(&tree[current], &curr_val, new_val));
-        } while (upper_bound < LEVEL(current) && !is_occ_buddy(new_val, child));
+                        new_val = nb_unmark(curr_val, child);
+                } while (!BCAS(&nb_tree[current], &curr_val, new_val));
+        } while (upper_bound < nb_level(current) &&
+                        !nb_is_occ_buddy(new_val, child));
 }
 
-void __freenode(uint32_t node, uint32_t upper_bound)
+void __nb_freenode(uint32_t node, uint32_t upper_bound)
 {
         /* TODO: should I check for double frees? */
-        if (is_free(tree[node])) {
+        if (nb_is_free(nb_tree[node])) {
                 return;
         }
 
@@ -199,18 +229,19 @@ void __freenode(uint32_t node, uint32_t upper_bound)
         uint32_t current = node >> 1;
         uint32_t child = node;
 
-        while (base_level < LEVEL(child)) {
+        while (nb_base_level < nb_level(child)) {
                 uint8_t curr_val = 0;
                 uint8_t new_val = 0;
                 uint8_t old_val = 0;
                 
                 do {
-                        curr_val = tree[current];
-                        new_val = set_coal(curr_val, child);
-                        old_val = VCAS(&tree[current], &curr_val, new_val);
+                        curr_val = nb_tree[current];
+                        new_val = nb_set_coal(curr_val, child);
+                        old_val = VCAS(&nb_tree[current], &curr_val, new_val);
                 } while (old_val != curr_val);
                 
-                if (is_occ_buddy(old_val, child) && !is_coal_buddy(old_val, child)) {
+                if (nb_is_occ_buddy(old_val, child) && 
+                        nb_is_coal_buddy(old_val, child)) {
                         break;
                 }
 
@@ -219,39 +250,126 @@ void __freenode(uint32_t node, uint32_t upper_bound)
         }
 
         /* Phase 2. Mark the node as free */
-        tree[node] = 0;
+        nb_tree[node] = 0;
 
         /* Phase 3. Propagate node release upward and possibly merge buddies */
-        if (LEVEL(node) != base_level) {
-                __unmark(node, upper_bound);
+        if (nb_level(node) != nb_base_level) {
+                __nb_unmark(node, upper_bound);
         }
 }
 
 void nb_free(void *addr)
 {
-        /* Range check (is this necessary?) */
-        uint64_t u_addr = (uint64_t) addr;
-        if (u_addr < base_address || (base_address + total_memory) < u_addr) {
+        if (!addr) {
                 return;
         }
 
-        uint32_t n = (u_addr - base_address) / min_size;
-        __freenode(index[n], base_level);
-        FAD(&release_count, 1);
+        uint32_t n = ((uint64_t) addr - nb_base_address) / NB_MIN_SIZE;
+        __nb_freenode(nb_index[n], nb_base_level);
+
+        FAD(&nb_release_count, 1);
+        FAD(&nb_stat_alloc_blocks[nb_depth - nb_level(nb_index[n])], -1);
 }
 
-uint64_t __block_size(uint32_t level)
+/* ------------------------------ STATISTICS -------------------------------- */
+
+uint64_t nb_stat_min_size()
 {
-        return EXP2(depth - level) * min_size;
+        return NB_MIN_SIZE;
 }
 
-void __clean_block(void* addr, uint64_t size)
+uint32_t nb_stat_max_order()
 {
-        if (!addr || !size) {
-                return;
+        return NB_MAX_ORDER;
+}
+
+uint64_t nb_stat_tree_size()
+{
+        return nb_tree_size;
+}
+
+uint64_t nb_stat_index_size()
+{
+        return nb_index_size;
+}
+
+uint32_t nb_stat_depth()
+{
+        return nb_depth;
+}
+
+uint32_t nb_stat_base_level()
+{
+        return nb_base_level;
+}
+
+uint64_t nb_stat_max_size()
+{
+        return nb_max_size;
+}
+
+uint32_t nb_stat_release_count()
+{
+        return nb_release_count;
+}
+
+
+uint64_t nb_stat_total_memory()
+{
+        return nb_total_memory;
+}
+
+uint64_t nb_stat_used_memory()
+{
+        uint64_t used_memory = 0;
+
+        for (uint32_t i = 0; i <= NB_MAX_ORDER; i++) {
+                used_memory += nb_stat_alloc_blocks[i] * nb_stat_block_size(i);
         }
 
-        memset(addr, 0x0, size);
+        return used_memory;
 }
 
-#endif /* NBBS_H */
+uint64_t nb_stat_block_size(uint32_t order)
+{
+        if (NB_MAX_ORDER < order) {
+                return 0;
+        }
+
+        return EXP2(order) * NB_MIN_SIZE;
+}
+
+uint64_t nb_stat_total_blocks(uint32_t order)
+{
+        if (NB_MAX_ORDER < order) {
+                return 0;
+        }
+
+        return nb_total_memory / nb_stat_block_size(order);
+}
+
+uint64_t nb_stat_used_blocks(uint32_t order)
+{
+        if (NB_MAX_ORDER < order) {
+                return 0;
+        }
+        
+        return nb_stat_alloc_blocks[order];
+}
+
+
+uint8_t nb_stat_occupancy_map(uint8_t *buff, uint32_t order)
+{
+        if (!buff || NB_MAX_ORDER < order) {
+                return 1;
+        }
+
+        uint32_t start_node = EXP2(nb_depth - order);
+        uint32_t end_node = EXP2(nb_depth - order + 1);
+
+        for (uint32_t i = start_node; i < end_node; i++) {
+                buff[i - start_node] = !nb_is_free(nb_tree[i]);
+        }
+
+        return 0;
+}
